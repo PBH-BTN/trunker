945159d27f3c0e1439795d3fd4f9d9dbc0b05a8b
Gaojianli <admin@gaojianli.me>
2024-12-19 20:05:02 +0800

    default enable optimize

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cp script/* output/
 chmod +x output/bootstrap.sh
 cp conf/* output/conf/
 export GOEXPERIMENT=arenas
-if [ "$BUILD_TYPE" == "online" ]; then
+if [ "$BUILD_TYPE" != "test" ]; then
     go build -ldflags="-w -s" -o output/bin/${RUN_NAME}
 else
     go build -gcflags="all=-N -l" -o output/bin/${RUN_NAME}
