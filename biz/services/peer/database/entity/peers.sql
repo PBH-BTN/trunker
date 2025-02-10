2900bca4ccdf8641343915de0b30da9b6371dccc
Gaojianli <admin@gaojianli.me>
2025-02-10 20:03:58 +0800

    有可能为空

--- biz/services/peer/database/entity/peers.sql
+++ biz/services/peer/database/entity/peers.sql
@@ -2,7 +2,7 @@ CREATE TABLE `peers` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
   `info_hash` varchar(40) NOT NULL COMMENT 'Info hash',
   `peer_id` varchar(40) DEFAULT NULL,
-  `ip` varbinary(16) NOT NULL,
+  `ip` varbinary(16) DEFAULT NULL,
   `ipv4` varbinary(4) DEFAULT NULL,
   `ipv6` varbinary(16) DEFAULT NULL,
   `client_ip` varbinary(16) NOT NULL COMMENT 'The ip from the heep client',
@@ -18,5 +18,5 @@ CREATE TABLE `peers` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_info_hash_peer_id` (`peer_id`,`info_hash`),
   KEY `idx_event` (`event`),
-  KEY `idx_last_seen` (`last_seen` DESC)
+  KEY `idx_last_seen` (`last_seen`)
 ) ENGINE=rocksdb DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Peers table';
