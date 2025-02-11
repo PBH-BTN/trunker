5d6aa2840c94cc07b8cfef0b1b7d33f7bf8ca688
Gaojianli <admin@gaojianli.me>
2025-02-11 14:56:06 +0800

    fix graceful exit

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -5,4 +5,10 @@ RUN_NAME="pbh.btn.trunker"
 
 #inject some environment variables
 
-exec bin/${RUN_NAME}
+exec bin/${RUN_NAME} &
+pid=$!
+# Trap the SIGTERM signal and forward it to the main process
+trap 'kill -SIGINT $pid; wait $pid' SIGTERM
+
+# Wait for the main process to complete
+wait $pidh
