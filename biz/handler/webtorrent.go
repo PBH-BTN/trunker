19f6946c3acbe56ad1c78fecb2fafdae33890dc8
Gaojianli <admin@gaojianli.me>
2025-02-24 16:30:30 +0800

    fix utf8

--- biz/handler/webtorrent.go
+++ biz/handler/webtorrent.go
@@ -93,6 +93,7 @@ func handleWSAnswer(ctx context.Context, msg []byte) error {
 	if err != nil {
 		return err
 	}
+	infoHash = string(transUTF8To9959_1(conv.UnsafeStringToBytes(infoHash)))
 	peerIdRaw, err := sonic.Get(msg, "peer_id")
 	if err != nil {
 		return err
