a7188767b2d2d172bb9bcd8cc7b3e41ae9c498f3
Gaojianli <admin@gaojianli.me>
2025-02-25 11:59:02 +0800

    add metrics port

--- init.go
+++ init.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/PBH-BTN/trunker/biz/config"
 	"github.com/PBH-BTN/trunker/biz/services/peer"
 	"github.com/PBH-BTN/trunker/service/metrics"
 	"github.com/PBH-BTN/trunker/service/mq/producer"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	hertzConfig "github.com/cloudwego/hertz/pkg/common/config"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	prometheus "github.com/hertz-contrib/monitor-prometheus"
 )
 
 func Init() {
@@ -15,4 +21,25 @@ func Init() {
 	metrics.Init()
 	//cache.Init()
 	peer.InitPeerManager()
+}
+
+func getServerOption() []hertzConfig.Option {
+	options := []hertzConfig.Option{
+		server.WithHostPorts(config.AppConfig.Tracker.HostPorts),
+		server.WithExitWaitTime(time.Minute),
+	}
+	if config.AppConfig.Tracker.EnableMetrics {
+		options = append(options, server.WithTracer(
+			prometheus.NewServerTracer(config.AppConfig.Tracker.MetricsHostPorts, "/metrics",
+				prometheus.WithDefaultServerMux(true),
+				prometheus.WithEnableGoCollector(true),
+				prometheus.WithRegistry(metrics.GetRegistry()),
+			),
+		))
+		hlog.Info("metrics is listen at ", config.AppConfig.Tracker.MetricsHostPorts)
+	}
+	if config.AppConfig.Tracker.UseUnixSocket {
+		options = append(options, server.WithNetwork("unix"))
+	}
+	return options
 }
