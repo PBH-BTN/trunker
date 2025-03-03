5949396fdf84bdbfac1f02b73dfdd9d908d407f0
Gaojianli <admin@gaojianli.me>
2025-03-03 18:54:28 +0800

    use cgo

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ ARG COMMIT_SHA
 ARG TARGETPLATFORM
 WORKDIR /build
 COPY . .
-RUN export GOARCH=${TARGETPLATFORM#*/}  && sh build.sh $COMMIT_SHA
+RUN apk add build-base pkgconfig re2-dev
+RUN export GOARCH=${TARGETPLATFORM#*/} && sh build.sh $COMMIT_SHA
 
 FROM alpine
 WORKDIR /app
 COPY --from=build /build/output .
+RUN apk add --no-cache re2
 ENTRYPOINT ["./bootstrap.sh"]
--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ mkdir -p output/bin output/conf
 cp script/* output/
 chmod +x output/bootstrap.sh
 cp conf/* output/conf/
-export GOEXPERIMENT=arenas
 if [ "$BUILD_TYPE" != "test" ]; then
-    go build -trimpath -ldflags="-w -s -X 'main.Commit=$1'" -tags="gc_opt poll_opt" -o output/bin/${RUN_NAME}
+    CGO_ENABLED=1 go build -trimpath -ldflags="-w -s -X 'main.Commit=$1'" -tags="gc_opt poll_opt re2_cgo" -o output/bin/${RUN_NAME}
 else
     go build -trimpath -gcflags="all=-N -l -X 'main.Commit=$1'" -o output/bin/${RUN_NAME}
 fi
