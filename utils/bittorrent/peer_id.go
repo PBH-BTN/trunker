c05ee80cc93593042bd1b342bb2e15841afc3714
Gaojianli <admin@gaojianli.me>
2025-03-03 20:12:24 +0800

    无奇不有

--- utils/bittorrent/peer_id.go
+++ utils/bittorrent/peer_id.go
@@ -1,12 +1,18 @@
 package bittorrent
 
 import (
+	"strings"
+
 	"github.com/cloudwego/hertz/pkg/common/hlog"
 	regexp "github.com/wasilibs/go-re2"
 )
 
-var commonClients = regexp.MustCompile(`^-.+-`)                // common clients, use for qbitorrent, utorrent, vuze, bittorrent, etc
-var btspritClients = regexp.MustCompile(`^-[a-zA-Z]+[0-9]{4}`) // btsprit-like clients, no second -
+var (
+	commonClients  = regexp.MustCompile(`^-.+-`)                     // common clients, use for qbitorrent, utorrent, vuze, bittorrent, etc
+	btSpritClients = regexp.MustCompile(`^-[a-zA-Z]+[0-9]{4}`)       // btsprit-like clients, no second -
+	aria2Clients   = regexp.MustCompile(`^A2-[0-9]+-[0-9]+-[0-9]+-`) // Aria2 clients A2-1-35-0-xxxx
+	middleDash     = regexp.MustCompile(`^[a-zA-Z]+[0-9]+-`)         // middle dash clients TIX0332-77wfm2rcxovo
+)
 
 // ParsePeerID Parse the client name from the peer_id
 func ParsePeerID(peerIdRaw string) string {
@@ -15,11 +21,22 @@ func ParsePeerID(peerIdRaw string) string {
 	}
 	common := commonClients.FindStringSubmatch(peerIdRaw)
 	if len(common) > 0 {
-		return common[0]
+		return common[0][:len(common[0])-1]
 	}
-	slash := btspritClients.FindStringSubmatch(peerIdRaw)
-	if len(slash) > 0 {
-		return slash[0]
+	btSprit := btSpritClients.FindStringSubmatch(peerIdRaw)
+	if len(btSprit) > 0 {
+		return btSprit[0]
+	}
+	aria2 := aria2Clients.FindStringSubmatch(peerIdRaw)
+	if len(aria2) > 0 {
+		return aria2[0][:len(aria2[0])-1]
+	}
+	middle := middleDash.FindStringSubmatch(peerIdRaw)
+	if len(middle) > 0 {
+		return middle[0][:len(middle[0])-1]
+	}
+	if strings.HasPrefix(peerIdRaw, "FD6") { //FD68Ki0o~Jd0mWb(GCY5
+		return "FD6"
 	}
 	hlog.Info("unknown peer id: ", peerIdRaw)
 	return "unknown"
--- utils/bittorrent/peer_id_test.go
+++ utils/bittorrent/peer_id_test.go
@@ -16,12 +16,12 @@ func TestParsePeerID(t *testing.T) {
 		{
 			name:     "validPeerIDWithDashPrefix",
 			peerID:   "-qB452B-w(33PK*XDMdD",
-			expected: "-qB452B-",
+			expected: "-qB452B",
 		},
 		{
 			name:     "validPeerIDWithDifferentDashPrefix",
 			peerID:   string(conv.TransUTF8To8859_1([]byte("-BC0211-\f\u00a9\u00c4Q\u0000 \u00a7\u000f\u00d1\u00cc\u0084\u0014"))),
-			expected: "-BC0211-",
+			expected: "-BC0211",
 		},
 		{
 			name:     "validPeerIDWithSlashPrefix",
@@ -42,6 +42,31 @@ func TestParsePeerID(t *testing.T) {
 			name:     "peerIDWithLessThanEightCharacters",
 			peerID:   "qB452B",
 			expected: "",
+		},
+		{
+			name:     "aria21",
+			peerID:   "A2-1-35-0-��)\u0014\u0015��\u0006�",
+			expected: "A2-1-35-0",
+		},
+		{
+			name:     "aria22",
+			peerID:   "A2-1-34-0-��W��f�F",
+			expected: "A2-1-34-0",
+		},
+		{
+			name:     "middleDash1",
+			peerID:   "TIX0332-77wfm2rcxovo",
+			expected: "TIX0332",
+		},
+		{
+			name:     "middleDash2",
+			peerID:   "TIX0262-e1f1a6e7g7f3",
+			expected: "TIX0262",
+		},
+		{
+			name:     "FD6",
+			peerID:   "FD68Ki0o~Jd0mWb(GCY5",
+			expected: "FD6",
 		},
 	}
 
