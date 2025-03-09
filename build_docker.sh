fe623fd352e0a7f4a90abe6544a9113e94ca8841
Gaojianli <admin@gaojianli.me>
2025-03-09 11:26:34 +0800

    add build_docker.sh

---
+++ build_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+COMMIT_SHA=$1
+VERSION=$2
+TARGETPLATFORM=$3
+if [ "$TARGETPLATFORM" = "linux/arm64" ] ; then
+  dpkg --add-architecture arm64
+  apt-get update
+  apt-get install -y crossbuild-essential-arm64 libre2-dev:arm64
+  export GOARCH=arm64
+  export CC=aarch64-linux-gnu-gcc
+  export CXX=aarch64-linux-gnu-g++
+  export AR=aarch64-linux-gnu-ar
+  export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
+else
+  apt-get install -y build-essential libre2-dev
+fi
+./build.sh $VERSION $COMMIT_SHA
