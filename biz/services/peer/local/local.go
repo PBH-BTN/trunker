72cd8edf12421ce800c088631caaff3e7aefcd8d
Gaojianli <admin@gaojianli.me>
2025-03-09 17:10:46 +0800

    same peer is a low possibility event

--- biz/services/peer/local/local.go
+++ biz/services/peer/local/local.go
@@ -118,9 +118,6 @@ func (m *Manager) HandleAnnouncePeer(ctx context.Context, req *model.AnnounceReq
 	var oldestPeer *common.Peer
 	shouldEject := root.peerMap.Len() > config.AppConfig.Tracker.Memory.MaxPeersPerTorrent
 	root.peerMap.Range(func(_ string, value *common.Peer) bool {
-		if value.ID == peer.ID {
-			return true
-		}
 		if time.Now().Add(time.Duration(-1*config.AppConfig.Tracker.TTL) * time.Second).After(value.LastSeen) {
 			// timeout!
 			timeoutPeer = append(timeoutPeer, value)
@@ -150,6 +147,9 @@ func (m *Manager) HandleAnnouncePeer(ctx context.Context, req *model.AnnounceReq
 		}
 		if len(resp) >= req.NumWant {
 			return false
+		}
+		if value.ID == peer.ID {
+			return true
 		}
 		resp = append(resp, value)
 		return true
