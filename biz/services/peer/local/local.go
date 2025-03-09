3dddde428d7f2dc34b288146f31a384f038e4d31
Gaojianli <admin@gaojianli.me>
2025-03-09 17:42:47 +0800

    correct len()

--- biz/services/peer/local/local.go
+++ biz/services/peer/local/local.go
@@ -112,7 +112,7 @@ func (m *Manager) HandleAnnouncePeer(ctx context.Context, req *model.AnnounceReq
 		}
 	})
 	// get return
-	resp := make([]*common.Peer, 0, min(root.peerMap.Len(), req.NumWant))
+	resp := make([]*common.Peer, 0, min(root.peerMap.Len(), req.NumWant, config.AppConfig.Tracker.Memory.MaxPeersPerTorrent))
 	timeoutPeer := make([]*common.Peer, 0)
 	var oldestTime *time.Time
 	var oldestPeer *common.Peer
--- utils/collections/mapx/sync.go
+++ utils/collections/mapx/sync.go
@@ -4,22 +4,17 @@ package mapx
 
 import (
 	"sync"
-	"sync/atomic"
 )
 
 // Map a wrapper for sync.Map to support generic
 type Map[V any] struct {
-	m      sync.Map
-	length atomic.Int64
+	m sync.Map
 }
 
 var _ SyncStringMap[int] = &Map[int]{}
 
 func (m *Map[V]) Delete(key string) bool {
 	_, ok := m.LoadAndDelete(key)
-	if ok {
-		m.length.Add(-1)
-	}
 	return ok
 }
 func (m *Map[V]) Load(key string) (value V, ok bool) {
@@ -33,28 +28,19 @@ func (m *Map[V]) LoadAndDelete(key string) (value V, loaded bool) {
 	v, loaded := m.m.LoadAndDelete(key)
 	if !loaded {
 		return value, loaded
-	} else {
-		m.length.Add(-1)
-		return v.(V), loaded
 	}
+	return v.(V), loaded
+
 }
 func (m *Map[V]) LoadOrStore(key string, value V) (actual V, loaded bool) {
 	a, loaded := m.m.LoadOrStore(key, value)
-	if !loaded {
-		m.length.Add(1)
-	}
 	return a.(V), loaded
 }
 func (m *Map[V]) Range(f func(key string, value V) bool) {
 	m.m.Range(func(key, value any) bool { return f(key.(string), value.(V)) })
 }
 func (m *Map[V]) Store(key string, value V) {
-	_, loaded := m.m.LoadOrStore(key, value)
-	if loaded {
-		m.m.Store(key, value)
-	} else {
-		m.length.Add(1)
-	}
+	m.m.Store(key, value)
 }
 
 func (m *Map[V]) LoadOrStoreLazy(key string, lazy func() V) (actual V, loaded bool) {
@@ -62,16 +48,18 @@ func (m *Map[V]) LoadOrStoreLazy(key string, lazy func() V) (actual V, loaded bo
 	if !loaded {
 		v = lazy()
 		a, loaded := m.m.LoadOrStore(key, v)
-		if !loaded {
-			m.length.Add(1)
-		}
 		return a.(V), loaded
 	}
 	return v.(V), loaded
 }
 
 func (m *Map[V]) Len() int {
-	return int(m.length.Load())
+	count := 0
+	m.m.Range(func(_, _ any) bool {
+		count++
+		return true
+	})
+	return count
 }
 
 func New[V any]() SyncStringMap[V] {
