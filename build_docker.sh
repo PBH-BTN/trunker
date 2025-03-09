c9c38a3c9119ad4abb9c42e81f725a4d2e449d1c
Gaojianli <admin@gaojianli.me>
2025-03-09 11:27:10 +0800

    apt update

--- build_docker.sh
+++ build_docker.sh
@@ -12,6 +12,7 @@ if [ "$TARGETPLATFORM" = "linux/arm64" ] ; then
   export AR=aarch64-linux-gnu-ar
   export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
 else
+  apt-get update
   apt-get install -y build-essential libre2-dev
 fi
 ./build.sh $VERSION $COMMIT_SHA
