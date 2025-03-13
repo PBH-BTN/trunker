86e2e800fcccb59264b75002dceed01b751afaba
Gaojianli <admin@gaojianli.me>
2025-03-13 16:00:25 +0800

    fix log

--- biz/services/peer/local/local.go
+++ biz/services/peer/local/local.go
@@ -184,7 +184,7 @@ func (m *Manager) HandleAnnouncePeer(ctx context.Context, req *model.AnnounceReq
 		return true
 	})
 	if root.peerMap[root.currentActive].Len() > config.AppConfig.Tracker.Memory.MaxPeersPerTorrent/2 { // reach max, start to eject
-		hlog.CtxDebugf(ctx, "[info_hash %s]active set full, currentActive %d, size: 1: %d 2:%d 3:%d", hex.EncodeToString(conv.UnsafeStringToBytes(req.InfoHash)), root.currentActive, root.peerMap[0].Len(), root.peerMap[1].Len(), root.peerMap[2].Len())
+		hlog.CtxDebugf(ctx, "[info_hash %s]active set full, currentActive %d, size: 0: %d 1:%d 2:%d", hex.EncodeToString(conv.UnsafeStringToBytes(req.InfoHash)), root.currentActive, root.peerMap[0].Len(), root.peerMap[1].Len(), root.peerMap[2].Len())
 		current := root.currentActive
 		if atomic.CompareAndSwapUint32(&root.currentActive, current, (current+1)%3) { // write head switch to next
 			hlog.CtxDebugf(ctx, "[info_hash %s] active set swapped! current:%d", hex.EncodeToString(conv.UnsafeStringToBytes(req.InfoHash)), root.currentActive)
