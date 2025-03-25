22853625f7375d197fc3acc1b4bc30a62a88e6f1
Gaojianli <admin@gaojianli.me>
2025-03-25 13:51:04 +0800

    完善错误日志

--- service/cache/utils.go
+++ service/cache/utils.go
@@ -2,20 +2,29 @@ package cache
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/PBH-BTN/trunker/utils/conv"
 	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/redis/go-redis/v9"
 	"github.com/shamaton/msgpack/v2"
 )
 
 func Get[T any](ctx context.Context, key string) (*T, bool) {
-	str := Client.Get(ctx, key).Val()
+	str, err := Client.Get(ctx, key).Result()
+	if errors.Is(err, redis.Nil) {
+		return nil, false
+	}
+	if err != nil {
+		hlog.CtxErrorf(ctx, "failed to get value from redis, key: %s, error: %s", key, err)
+		return nil, false
+	}
 	if str == "" {
 		return nil, false
 	}
 	var value T
-	err := msgpack.Unmarshal(conv.UnsafeStringToBytes(str), &value)
+	err = msgpack.Unmarshal(conv.UnsafeStringToBytes(str), &value)
 	if err != nil {
 		hlog.CtxWarnf(ctx, "failed to unmarshal value, key: %s, value: %s , error:%s", key, str, err)
 		return nil, false
@@ -43,6 +52,14 @@ func Del(ctx context.Context, key string) error {
 }
 
 func Set[T any](ctx context.Context, key string, value T, expired time.Duration) error {
-	str, _ := msgpack.Marshal(value)
-	return Client.Set(ctx, key, str, expired).Err()
+	str, err := msgpack.Marshal(value)
+	if err != nil {
+		hlog.CtxErrorf(ctx, "failed to marshal value, key: %s, value: %v, error: %s", key, value, err)
+		return err
+	}
+	err = Client.Set(ctx, key, str, expired).Err()
+	if err != nil {
+		hlog.CtxErrorf(ctx, "failed to set value to redis, key: %s, value: %v, error: %s", key, value, err)
+	}
+	return err
 }
