5bf97617427f7d2246b5b7ae6b32d561d8b288f8
Gaojianli <admin@gaojianli.me>
2025-11-20 14:12:28 +0800

    fix asm.h

--- build_docker.sh
+++ build_docker.sh
@@ -3,17 +3,19 @@ make install_tool && make update_idl
 if [ "$TARGETPLATFORM" = "linux/arm64" ] ; then
   dpkg --add-architecture arm64
   apt-get update
-  apt-get install -y crossbuild-essential-arm64 libre2-dev:arm64 clang llvm linux-headers-generic:arm64 libbpf-dev:arm64
+  apt-get install -y crossbuild-essential-arm64 libre2-dev:arm64 clang llvm libbpf-dev:arm64
   export GOARCH=arm64
   export CC=aarch64-linux-gnu-gcc
   export CXX=aarch64-linux-gnu-g++
   export AR=aarch64-linux-gnu-ar
   export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
+  ln -s /usr/include/aarch64-linux-gnu/asm /usr/include/asm
   go generate biz/services/peer/mux_local/ban/xdp/filter.go
   # Generate eBPF code (will be no-op on non-Linux platforms due to build tags)
 else
   apt-get update
-  apt-get install -y build-essential libre2-dev clang llvm linux-headers-generic libbpf-dev
+  apt-get install -y build-essential libre2-dev clang llvm libbpf-dev
+  ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
   go generate biz/services/peer/mux_local/ban/xdp/filter.go
   export GOAMD64=v4
 fi
