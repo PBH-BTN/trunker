4e5e1d6d822adb09c5413c417ecc4524b202ad78
Gaojianli <admin@gaojianli.me>
2025-11-20 14:29:58 +0800

    稍后设置编译环境变量

--- build_docker.sh
+++ build_docker.sh
@@ -4,14 +4,13 @@ if [ "$TARGETPLATFORM" = "linux/arm64" ] ; then
   dpkg --add-architecture arm64
   apt-get update
   apt-get install -y crossbuild-essential-arm64 libre2-dev:arm64 clang llvm libbpf-dev:arm64
+  ln -s /usr/include/aarch64-linux-gnu/asm /usr/include/asm
+  go generate biz/services/peer/mux_local/ban/xdp/filter.go
   export GOARCH=arm64
   export CC=aarch64-linux-gnu-gcc
   export CXX=aarch64-linux-gnu-g++
   export AR=aarch64-linux-gnu-ar
-  export AS=aarch64-linux-gnu-as
   export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
-  ln -s /usr/include/aarch64-linux-gnu/asm /usr/include/asm
-  go generate biz/services/peer/mux_local/ban/xdp/filter.go
 else
   apt-get update
   apt-get install -y build-essential libre2-dev clang llvm libbpf-dev
